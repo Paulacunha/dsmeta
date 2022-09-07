10f7cba7b6049c5a4f80292529c38de0df388e9b
Paula Cunha <paulathcunha@gmail.com>
2022-09-07 18:41:07 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import NotificationBotton from "./components/NotificationButton"
 
 
 function App() {
-return(
-  <h1>Olá Mundo</h1>
-)
+  return (
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationBotton />
+      <NotificationBotton />
+    </>
+  )
 }
 
 export default App
